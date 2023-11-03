6a4ce254e75e1c33c5723d1863dd864b49ac8955
Andrew Lackey <77242875+ALackey-ai@users.noreply.github.com>
2023-11-03 14:01:52 -0600

    Create index.php
    Added some starter code examples from school website project

---
+++ index.php
@@ -0,0 +1,32 @@
+//starter code
+
+<?php
+// php code to run if needed
+?>
+<html>
+  //reference css files as needed
+<!--     <link rel = "stylesheet" href = "style_home.css">
+    <link rel = "stylesheet" href = "style_general_case_top_bar.css">
+    <link rel = "stylesheet" href = "style_login_signup_buttons.css">
+    <link rel = "stylesheet" href = "style_Login_Signup.css"> -->
+  
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+    </style>
+
+  //add some scripts as needed
+<!--     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script type = "text/javascript" src="buttonFunction.js"></script>
+    <script type = "text/javascript" src="renderCorrectInfoBasedOnAuth.js"></script> -->
+    
+
+    <body>
+        <div class = "wrapper">
+          <div class = "top_bar"> 
+
+            </div>
+        </div>
+    </body>
+</html>
+            
