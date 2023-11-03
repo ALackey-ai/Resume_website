6fec86d1781f830852f74bf04a06cf7bc22d54e0
Andrew Lackey <andrewlackey@u.boisestate.edu>
2023-11-03 17:17:02 -0600

    Added some simple css and js, trying to get tsparticles to work

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-//starter code
+<!-- starter code -->
 
 <?php
 // php code to run if needed
 ?>
 <html>
-  //reference css files as needed
+  <link rel = "stylesheet" href = "index.css">
+  <!-- // reference css files as needed -->
 <!--     <link rel = "stylesheet" href = "style_home.css">
-    <link rel = "stylesheet" href = "style_general_case_top_bar.css">
+    <link rel = "stylesheet" href = "index.css">
     <link rel = "stylesheet" href = "style_login_signup_buttons.css">
     <link rel = "stylesheet" href = "style_Login_Signup.css"> -->
   
@@ -15,17 +16,23 @@
         @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     </style>
 
-  //add some scripts as needed
+  <!-- // add some scripts as needed -->
 <!--     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script type = "text/javascript" src="buttonFunction.js"></script>
     <script type = "text/javascript" src="renderCorrectInfoBasedOnAuth.js"></script> -->
     
 
     <body>
-        <div class = "wrapper">
-          <div class = "top_bar"> 
-            Hellooo
-            </div>
+        <div id = "topWrap" class = "wrapper">
+        <div id="tsparticles"></div>
+
+          <!-- script -->
+          <script src="https://cdn.jsdelivr.net/npm/tsparticles@1.16.2/dist/tsparticles.min.js" integrity="sha256-2Sly/Hf9UP7YBu6LEPpn/6rxMZR5EeGq89mBa4G5+YA=" crossorigin="anonymous"></script>
+          <script src="app.js"></script>
+
+        <div class = "top_bar"> 
+          Hellooo
+        </div>
         </div>
     </body>
 </html>
